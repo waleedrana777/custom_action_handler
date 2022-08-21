3da7cf8e2cd815d38a2f1feb35fe650f4aa79f51
waleedrana777 <37755440+waleedrana777@users.noreply.github.com>
2022-08-21 18:42:32 +0200

    all

--- firebase/db.js
+++ firebase/db.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: 'process.env.FIREBASE_API_KEY',
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
