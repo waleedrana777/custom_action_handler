a1e3a6488e74ec1d407d24a96669728de4532941
waleedrana777 <37755440+waleedrana777@users.noreply.github.com>
2022-08-21 20:49:00 +0200

    yes

--- firebase/db.js
+++ firebase/db.js
@@ -1,23 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-const auth = null;
+
+var app = null;
+var auth = null;
 
 try {
     const firebaseConfig = {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     };
-
-    const app = initializeApp(firebaseConfig);
+    if (!app) {
+        app = initializeApp(firebaseConfig);
+    }
     auth = getAuth(app);
 }
 catch (error) {
-    console.log(error);
+    console.log(process.env.FIREBASE_API_KEY);
+    console.log(error.message);
 }
 
 export {
