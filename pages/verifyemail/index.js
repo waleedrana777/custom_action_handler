c67063e543c1a4bf6c0b158b8925099406868633
waleedrana777 <37755440+waleedrana777@users.noreply.github.com>
2022-08-25 10:29:46 +0200

    custom email api

--- pages/verifyemail/index.js
+++ pages/verifyemail/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
 import { checkActionCode, applyActionCode } from 'firebase/auth';
 import { ToastContainer, toast } from 'react-toastify';
